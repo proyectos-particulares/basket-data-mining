4772bf3c3f3e6a023588ff460082e08665f43023
Enrique Redondo Cortés <eredondoco.inf@upsa.es>
2024-08-20 18:34:49 +0200

    feat: crear script de ejecución automática de JS
    Ejecuta código de JS en la consola de una URL dada y devuelve el resultado

---
+++ utils/python/ejecutar_script_js_en_url.py
@@ -0,0 +1,34 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def ejecutar_script_en_url(
+        url_pagina,
+        script_js,
+        tiempo_espera_carga=1,
+        tiempo_espera_script=0
+):
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+
+    driver = webdriver.Chrome(service=service, options=options)
+    try:
+        # Abrir la página web
+        driver.get(url_pagina)
+
+        print(f"- Cargando {url_pagina}...")
+        time.sleep(tiempo_espera_carga)
+
+        # Ejecutar el script JS
+        result = driver.execute_script(script_js)
+
+        print(f"- Ejecutando script JS...")
+        time.sleep(tiempo_espera_script)
+
+        return result
+    finally:
+        driver.quit()
